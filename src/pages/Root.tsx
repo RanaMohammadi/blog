2341907c41ea9c629ee30de8ca9ece6519435c3c
RanaMohammadi <itsmerana.m@gmail.com>
2024-01-21 22:21:59 +0100

    new post button added

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { Outlet, Link } from "react-router-dom";
+import { Box, AppBar, Toolbar, Typography, Stack, Button } from "@mui/material";
 const Root = () => {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h3">Blog</Typography>
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              flexGrow: 1,
+              color: "white",
+              padding: 7,
+            }}
+          >
+            <Typography variant="h3" color="inherit">
+              Blog
+            </Typography>
+          </Link>
+          <Link to="/new">
+            <Button style={{ color: "white" }}>Add new post</Button>
+          </Link>
         </Toolbar>
       </AppBar>
 
